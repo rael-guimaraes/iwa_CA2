8df98b483241e4d08360081c0bfb57d4855ba4cd
rael-guimaraes <raelguimaraes8@hotmail.com>
2022-05-03 13:34:02 +0000

    fixing index.js

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const   http = require("http"),
         logger = require("morgan"),
         express = require("express"),
+        bodyParcer = require("body-parser"),
         mongoose = require("mongoose");
 
 let app = express();
 let port = process.env.PORT || 8000;
 
-//dotenv.config();
-
-app.use(express.json());
-app.use(logger("tiny"));
 app.use(require('./routes'));
+app.use(bodyParcer.json());
+app.use(logger("tiny"));
+
+mongoose.connect('mongodb://localhost/test');
+
+mongoose.connection.on('error', (err) => {
+    console.log('Mongodb Error: ', err);
+    process.exit();
+});
+mongoose.connection.on('connected', () => {
+    console.log('Mongodb is connected');
+})
 
 mongoose.connect(process.env.dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then((result) => console.log('connected to db'))
